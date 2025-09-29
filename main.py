b088554ff37e4145d2f8047b47e6c1774ce707c2
Estevinchi <esteve.r@hotmail.com>
2025-09-29 09:41:49 +0200

    Implementacion de los datos a carpeta /data
    El cambio del jueves pasado que hicimos con Rosana, de añadir la carpeta de usuarios.json a Data. Y revisión de la función es_admin que ahora funciona.

---
+++ main.py
@@ -0,0 +1,8 @@
+from db import init_db
+from modules.usuarios import Usuario
+
+Usuario.es_admin("Esteve")
+
+
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,1 @@
-conn.execute(
-#   "INSERT INTO productos(nombre, precio, talla, cantidad) VALUES (?,?,?,?)",
-#   ('vestido de flores', 19.99, 'M', 20)
-# )
-# conn.commit()
+Esteve
